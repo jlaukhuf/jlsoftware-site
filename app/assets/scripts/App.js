42b18bd4a252be86ba9c0ad9f8f020de478295b7
Jason <jlaukhuf@parkcenter.org>
2018-04-06 09:47:15 -0400

    mobile menu appear complete master branch

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,18 @@
-import MobileMenu from './modules/MobileMenu';
+var $ = require('jquery');
+//var Person = require('./modules/Person');
+import Person from './modules/Person';
 
-var mobileMenu = new MobileMenu();
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes.")
+  }
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Adult("Jane Smith", "orange");
+jane.greet();
+jane.payTaxes();
+
+$("h1").remove();
